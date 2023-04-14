ac73975fabfab088ca56b16bf3bf062e5e3bfef8
Boris Diakur <borisdiakur@gmail.com>
2023-04-14 17:02:46 +0200

    test: fix form validation test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
+
+async function waitForLiquidToHydrate() {
+  return waitFor(() => {
+    // eslint-disable-next-line testing-library/no-node-access
+    Array.from(document.querySelectorAll('*'))
+      .filter((e) => e.tagName.startsWith('LD-'))
+      .forEach((component) => {
+        expect(component).toHaveClass('hydrated')
+      })
+  })
+}
 
 test('renders heading', async () => {
   render(<App />)
   const heading = screen.getByText('Liquid Sandbox App')
   expect(heading).toBeInTheDocument()
+})
+
+test('validates input', async () => {
+  render(<App />)
+
+  await waitForLiquidToHydrate()
+
+  const ldInputName = screen.getByPlaceholderText('e.g. Jason Parse')
+  expect(ldInputName).toBeInTheDocument()
+
+  const ldButtonSubmit = screen.getByText('Submit')
+  expect(ldButtonSubmit).toBeInTheDocument()
+  userEvent.click(ldButtonSubmit)
+
+  const form = await screen.findByTestId('form')
+  expect(form).toBeInTheDocument()
+
+  fireEvent.submit(form)
+
+  await screen.findByText('Your full name is required.')
 })
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,23 @@
 import '@testing-library/jest-dom'
 import { TextEncoder } from 'util'
+import { defineCustomElements } from '@emdgroup-liquid/liquid/dist/loader'
+
+// Prevent display of error message from JSDom CSS parser.
+// See https://github.com/jsdom/jsdom/issues/2177#issuecomment-376139329
+const originalConsoleError = console.error
+console.error = function (msg) {
+  if (msg.toString().startsWith('Error: Could not parse CSS stylesheet')) return
+  originalConsoleError(msg)
+}
+
+// @ts-ignore
+global.__LD_ASSET_PATH__ = '/'
+defineCustomElements()
+
 global.TextEncoder = TextEncoder
+global.requestAnimationFrame = (cb) => setTimeout(cb)
+global.fetch = () =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+    text: () => Promise.resolve(''),
+  } as Response)
